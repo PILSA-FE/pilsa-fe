47229577ffc86ef9a147b032679726139969c818
RYUJINSIK <it_jinsik@naver.com>
2024-01-30 20:25:21 +0900

    [#6] : 모달 컴포넌트 분리

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import "pretendard/dist/web/static/pretendard.css";
+// import "pretendard/dist/web/static/pretendard.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
---
+++ components/Modal/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./modal.module.css";
+
+const Modal = () => {
+  return (
+    <div>
+      <p>MODAL</p>
+    </div>
+  );
+};
+
+export default Modal;
