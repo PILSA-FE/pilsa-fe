7e1478835c6ec180b2bb6ee9be2add66793b0d52
RYUJINSIK <it_jinsik@naver.com>
2024-02-02 21:57:48 +0900

    [#9] : 필사 글 작성기능 구현

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,7 +1,92 @@
+"use client";
+import { useState } from "react";
 import WithHeaderLayout from "@/components/WithHeaderLayout";
 import Image from "next/image";
+import axios from "axios";
+import { blob } from "stream/consumers";
 
 const CreatePage = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    content: "",
+    file: null as any, // 추가: 파일 상태 추가
+    author: "",
+    publisher: "",
+    category: [] as any,
+  });
+  const [previewURL, setPreviewURL] = useState<any>(null);
+
+  const { title, content, file, author, publisher, category } = formData;
+
+  const handleInputChange = (event: {
+    target: { name: any; files?: any; value?: any };
+  }) => {
+    if (event.target.name === "filename") {
+      setFormData({ ...formData, file: event.target.files[0] });
+      const previewURL = URL.createObjectURL(event.target.files[0]);
+      setPreviewURL(URL.createObjectURL(event.target.files[0]));
+    } else if (event.target.name === "category") {
+      console.log("카테고리 입력");
+    } else {
+      const { name, value } = event.target;
+      setFormData({ ...formData, [name]: value });
+    }
+  };
+
+  const getImageUrl = async (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    const imageData = new FormData();
+    imageData.append("files", formData.file);
+    console.log(formData.file);
+
+    try {
+      const response = await axios.post(
+        "http://223.130.135.113:8080/api/v1/image",
+        imageData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      handleSubmit(response.data[0].imageUrl);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSubmit = async (imageUrl: string) => {
+    const requestBody = {
+      title: title,
+      author: author,
+      publisher: publisher,
+      textContents: content,
+      backgroundImageUrl: "",
+      backgroundColor: "",
+      categoryCd: [0],
+      images: [{ imageUrl: imageUrl, thumbnail: "Y", imageSeq: 0 }],
+    };
+    try {
+      const response = await axios.post(
+        "http://223.130.135.113:8080/api/v1/pilsa",
+        requestBody,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log(response.data);
+      alert("필사 작성이 완료되었습니다.");
+      // 이후 페이지 전환
+      document.location.href = "/";
+    } catch (error) {
+      console.error(error);
+    }
+
+    alert("stop");
+  };
+
   const categoryList = [
     "시",
     "산문",
@@ -21,10 +106,16 @@ const CreatePage = () => {
             type="text"
             className="text-xl bg-transparent border-none ring-0 focus:ring-0 focus:outline-none placeholder-[#999]"
             placeholder="제목"
+            name="title"
+            value={title}
+            onChange={handleInputChange}
           />
         </div>
         <hr className="my-4 border-[#EFEFEF]" />
-        <button className="inline-flex items-center justify-center w-full p-4 rounded-lg bg-[#EBEBEB] gap-x-1">
+        <label
+          htmlFor="imageUpload"
+          className="inline-flex items-center justify-center w-full p-4 rounded-lg bg-[#EBEBEB] gap-x-1 cursor-pointer"
+        >
           <Image
             src="/icons/camera_icon.png"
             width={16}
@@ -32,19 +123,38 @@ const CreatePage = () => {
             alt="camera"
           />
           <span className="text-[#6D6D6D] text-sm font-medium">
-            사진으로 필사 올리기 (1장)
+            {formData.file !== null
+              ? previewURL && (
+                  <Image
+                    src={previewURL}
+                    alt="미리보기"
+                    width={100}
+                    height={100}
+                  />
+                )
+              : "사진으로 필사 올리기 (1장)"}
           </span>
-        </button>
+          <input
+            type="file"
+            id="imageUpload"
+            className="hidden" // 파일 입력 요소를 숨깁니다.
+            name="filename"
+            accept="image/*"
+            onChange={handleInputChange} // 파일이 변경되었을 때 호출될 핸들러 함수
+          />
+        </label>
         <div className="my-3 rounded-xl py-5 px-4 h-[224px] w-full bg-[#F8F8F8]">
           <textarea
             name="content"
             id="content"
             className="bg-transparent w-full h-full resize-none focus:outline-none text-sm placeholder-[#999]"
             placeholder="필사 글 또는 이미지 필사의 내 생각을 입력해 주세요."
+            value={content}
+            onChange={handleInputChange}
           />
         </div>
         <p className="text-xs text-[#777]">
-          * 필사 글 또는 이미지 중 하나는 채워주세요! (워딩 변경 예정)
+          * 최소한, 필사 글 또는 이미지 중 하나는 채워 주세요!
         </p>
         <hr className="my-4 border-[#EFEFEF]" />
         <div className="relative rounded-xl py-5 px-3 bg-[#F8F8F8]">
@@ -52,6 +162,9 @@ const CreatePage = () => {
             type="text"
             className="text-sm bg-transparent border-none ring-0 focus:ring-0 focus:outline-none placeholder-[#999]"
             placeholder="출처(저자)"
+            name="author"
+            value={author}
+            onChange={handleInputChange}
           />
         </div>
         <div className="relative rounded-xl py-5 px-3 bg-[#F8F8F8] mt-4">
@@ -59,6 +172,9 @@ const CreatePage = () => {
             type="text"
             className="text-sm bg-transparent border-none ring-0 focus:ring-0 focus:outline-none placeholder-[#999]"
             placeholder="출판사"
+            name="publisher"
+            value={publisher}
+            onChange={handleInputChange}
           />
         </div>
         <hr className="my-4 border-[#EFEFEF]" />
@@ -94,6 +210,7 @@ const CreatePage = () => {
         <button
           type="submit"
           className="mt-10 mb-4 w-full py-4 rounded-lg text-white text-center bg-[#6D6D6D] text-sm font-bold"
+          onClick={getImageUrl}
         >
           필사 올리기
         </button>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import WithHeaderLayout from "@/components/WithHeaderLayout";
 
+import BottomSheet from "@/components/BottomSheet";
+
 export default function Home() {
   return (
     <WithHeaderLayout>
