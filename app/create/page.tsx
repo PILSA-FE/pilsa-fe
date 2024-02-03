84a3404b97be7bebb67683fdbdf6a9a710abdd3c
RYUJINSIK <it_jinsik@naver.com>
2024-02-03 20:29:08 +0900

    [#9] : 카드꾸미기 기능 + 배경이미지 추가

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import WithHeaderLayout from "@/components/WithHeaderLayout";
 import Image from "next/image";
 import axios from "axios";
-import { blob } from "stream/consumers";
+import BottomSheet from "@/components/BottomSheet";
 
 const CreatePage = () => {
   const [formData, setFormData] = useState({
@@ -118,151 +118,168 @@ const CreatePage = () => {
     "성경",
     "기타",
   ];
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   return (
-    <WithHeaderLayout>
-      <form action="" className="mt-4">
-        <div className="relative rounded-xl py-5 px-3 bg-[#F8F8F8]">
-          <input
-            type="text"
-            className="text-xl bg-transparent border-none ring-0 focus:ring-0 focus:outline-none placeholder-[#999]"
-            placeholder="제목"
-            name="title"
-            value={title}
-            onChange={handleInputChange}
-          />
-        </div>
-        <hr className="my-4 border-[#EFEFEF]" />
-
-        {formData.file !== null
-          ? previewURL && (
-              <div className="w-full h-[260px] relative ">
-                <Image
-                  src={previewURL}
-                  alt="미리보기"
-                  width={0}
-                  height={0}
-                  className="w-full h-[260px] absolute top-[-1px] rounded-lg object-cover"
-                />
-                <div
-                  className="cursor-pointer flex justify-start items-start absolute left-[300px] top-[210px] gap-2.5 p-2 rounded bg-white"
-                  style={{ boxShadow: "0px 0px 8px 0 rgba(0,0,0,0.15)" }}
-                  onClick={imageDelete}
-                >
-                  <Image
-                    src="/icons/trashcan_icon.png"
-                    width={24}
-                    height={24}
-                    alt="이미지삭제"
-                    className="flex-grow-0 flex-shrink-0 w-6 h-6 relative"
-                  />
-                </div>
-              </div>
-            )
-          : ""}
-
-        <div className="my-3 rounded-xl py-5 px-4 h-[224px] w-full bg-[#F8F8F8]">
-          <textarea
-            name="content"
-            id="content"
-            className="bg-transparent w-full h-full resize-none focus:outline-none text-sm placeholder-[#999]"
-            placeholder="필사 글 또는 이미지 필사의 내 생각을 입력해 주세요."
-            value={content}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="flex gap-1">
-          <button className="inline-flex items-center justify-center w-2/12 p-4 pt-5 rounded-lg bg-[url('/icons/palette_background.png')] gap-x-1 cursor-pointer text-white">
-            <Image
-              src="/icons/palette.png"
-              width={24}
-              height={24}
-              alt="카드꾸미기"
-              className="flex-grow-0 flex-shrink-0 w-6 h-6 relative"
-            />
-          </button>
-          <label
-            htmlFor="imageUpload"
-            className="inline-flex items-center justify-center w-10/12	 p-4 pt-5 rounded-lg bg-[#EBEBEB] gap-x-1 cursor-pointer"
-          >
-            <Image
-              src="/icons/camera_icon.png"
-              width={16}
-              height={16}
-              alt="camera"
-            />
-            <span className="text-[#6D6D6D] text-sm font-medium">
-              사진으로 필사 올리기 (1장)
-            </span>
+    <>
+      <WithHeaderLayout>
+        <form action="" className="mt-4">
+          <div className="relative rounded-xl py-5 px-3 bg-[#F8F8F8]">
             <input
-              type="file"
-              id="imageUpload"
-              className="hidden" // 파일 입력 요소를 숨깁니다.
-              name="filename"
-              accept="image/*"
-              onChange={handleInputChange} // 파일이 변경되었을 때 호출될 핸들러 함수
+              type="text"
+              className="text-xl bg-transparent border-none ring-0 focus:ring-0 focus:outline-none placeholder-[#999]"
+              placeholder="제목"
+              name="title"
+              value={title}
+              onChange={handleInputChange}
             />
-          </label>
-        </div>
-
-        <p className="text-xs text-[#777] pt-3">
-          * 최소한, 필사 글 또는 이미지 중 하나는 채워 주세요!
-        </p>
-        <hr className="my-4 border-[#EFEFEF]" />
-        <div className="relative rounded-xl py-5 px-3 bg-[#F8F8F8]">
-          <input
-            type="text"
-            className="text-sm bg-transparent border-none ring-0 focus:ring-0 focus:outline-none placeholder-[#999]"
-            placeholder="출처(저자)"
-            name="author"
-            value={author}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="relative rounded-xl py-5 px-3 bg-[#F8F8F8] mt-4">
-          <input
-            type="text"
-            className="text-sm bg-transparent border-none ring-0 focus:ring-0 focus:outline-none placeholder-[#999]"
-            placeholder="출판사"
-            name="publisher"
-            value={publisher}
-            onChange={handleInputChange}
-          />
-        </div>
-        <hr className="my-4 border-[#EFEFEF]" />
-        <div className="flex flex-col items-center justify-center gap-y-3">
-          <div className="flex items-center gap-x-0.5">
-            <p className="text-sm text-[#777] font-semibold">카테고리</p>
-            <span className="text-xs text-[#777]">(최대3개)</span>
           </div>
-          <ul className="flex flex-wrap items-center justify-center gap-2.5 px-2">
-            {categoryList.map((category) => (
-              <li
-                className={`px-3 py-1.5 border border-[#E3E3E3] rounded-[100px] cursor-pointer ${
-                  selectedCategories.includes(category) ? "bg-gray-500" : ""
-                }`}
-                key={category}
-                onClick={() => handleCategoryClick(category)}
-              >
-                <span
-                  className={`text-sm text-[#999] font-light ${
-                    selectedCategories.includes(category) ? "text-white" : ""
+          <hr className="my-4 border-[#EFEFEF]" />
+
+          {formData.file !== null
+            ? previewURL && (
+                <div className="w-full h-[260px] relative ">
+                  <Image
+                    src={previewURL}
+                    alt="미리보기"
+                    width={0}
+                    height={0}
+                    className="w-full h-[260px] absolute top-[-1px] rounded-lg object-cover"
+                  />
+                  <div
+                    className="cursor-pointer flex justify-start items-start absolute left-[300px] top-[210px] gap-2.5 p-2 rounded bg-white"
+                    style={{ boxShadow: "0px 0px 8px 0 rgba(0,0,0,0.15)" }}
+                    onClick={imageDelete}
+                  >
+                    <Image
+                      src="/icons/trashcan_icon.png"
+                      width={24}
+                      height={24}
+                      alt="이미지삭제"
+                      className="flex-grow-0 flex-shrink-0 w-6 h-6 relative"
+                    />
+                  </div>
+                </div>
+              )
+            : ""}
+
+          <div className="my-3 rounded-xl py-5 px-4 h-[224px] w-full bg-[#F8F8F8]">
+            <textarea
+              name="content"
+              id="content"
+              className="bg-transparent w-full h-full resize-none focus:outline-none text-sm placeholder-[#999]"
+              placeholder="필사 글 또는 이미지 필사의 내 생각을 입력해 주세요."
+              value={content}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="flex gap-1">
+            <button
+              type="button"
+              onClick={openModal}
+              className="inline-flex items-center justify-center w-2/12 p-4 pt-5 rounded-lg bg-[url('/icons/palette_background.png')] gap-x-1 cursor-pointer text-white"
+            >
+              <Image
+                src="/icons/palette.png"
+                width={24}
+                height={24}
+                alt="카드꾸미기"
+                className="flex-grow-0 flex-shrink-0 w-6 h-6 relative"
+              />
+            </button>
+            <label
+              htmlFor="imageUpload"
+              className="inline-flex items-center justify-center w-10/12	 p-4 pt-5 rounded-lg bg-[#EBEBEB] gap-x-1 cursor-pointer"
+            >
+              <Image
+                src="/icons/camera_icon.png"
+                width={16}
+                height={16}
+                alt="camera"
+              />
+              <span className="text-[#6D6D6D] text-sm font-medium">
+                사진으로 필사 올리기 (1장)
+              </span>
+              <input
+                type="file"
+                id="imageUpload"
+                className="hidden" // 파일 입력 요소를 숨깁니다.
+                name="filename"
+                accept="image/*"
+                onChange={handleInputChange} // 파일이 변경되었을 때 호출될 핸들러 함수
+              />
+            </label>
+          </div>
+
+          <p className="text-xs text-[#777] pt-3">
+            * 최소한, 필사 글 또는 이미지 중 하나는 채워 주세요!
+          </p>
+          <hr className="my-4 border-[#EFEFEF]" />
+          <div className="relative rounded-xl py-5 px-3 bg-[#F8F8F8]">
+            <input
+              type="text"
+              className="text-sm bg-transparent border-none ring-0 focus:ring-0 focus:outline-none placeholder-[#999]"
+              placeholder="출처(저자)"
+              name="author"
+              value={author}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="relative rounded-xl py-5 px-3 bg-[#F8F8F8] mt-4">
+            <input
+              type="text"
+              className="text-sm bg-transparent border-none ring-0 focus:ring-0 focus:outline-none placeholder-[#999]"
+              placeholder="출판사"
+              name="publisher"
+              value={publisher}
+              onChange={handleInputChange}
+            />
+          </div>
+          <hr className="my-4 border-[#EFEFEF]" />
+          <div className="flex flex-col items-center justify-center gap-y-3">
+            <div className="flex items-center gap-x-0.5">
+              <p className="text-sm text-[#777] font-semibold">카테고리</p>
+              <span className="text-xs text-[#777]">(최대3개)</span>
+            </div>
+            <ul className="flex flex-wrap items-center justify-center gap-2.5 px-2">
+              {categoryList.map((category) => (
+                <li
+                  className={`px-3 py-1.5 border border-[#E3E3E3] rounded-[100px] cursor-pointer ${
+                    selectedCategories.includes(category) ? "bg-gray-500" : ""
                   }`}
+                  key={category}
+                  onClick={() => handleCategoryClick(category)}
                 >
-                  {category}
-                </span>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <button
-          type="submit"
-          className="mt-10 mb-4 w-full py-4 rounded-lg text-white text-center bg-[#6D6D6D] text-sm font-bold"
-          onClick={getImageUrl}
-        >
-          필사 올리기
-        </button>
-      </form>
-    </WithHeaderLayout>
+                  <span
+                    className={`text-sm text-[#999] font-light ${
+                      selectedCategories.includes(category) ? "text-white" : ""
+                    }`}
+                  >
+                    {category}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <button
+            type="submit"
+            className="mt-10 mb-4 w-full py-4 rounded-lg text-white text-center bg-[#6D6D6D] text-sm font-bold"
+            onClick={getImageUrl}
+          >
+            필사 올리기
+          </button>
+        </form>
+      </WithHeaderLayout>
+      <BottomSheet
+        open={isModalOpen}
+        onClose={closeModal}
+        confirmButton="적용하기"
+        closeButton="취소"
+      />
+    </>
   );
 };
 export default CreatePage;
--- components/BottomSheet/index.tsx
+++ components/BottomSheet/index.tsx
@@ -1,14 +1,60 @@
-const BottomSheet = () => {
+import React, { ReactNode, useState } from "react";
+import ReactDOM from "react-dom";
+import styles from "./modal.module.css";
+import Image from "next/image";
+
+interface BottomSheetProps {
+  open: boolean;
+  onClose: () => void;
+  // title?: string;
+  // content?: string;
+  confirmButton?: ReactNode;
+  closeButton?: ReactNode;
+  confirmEvent?: () => void;
+}
+
+const BottomSheet = ({
+  open,
+  onClose,
+  // title,
+  // content,
+  confirmButton,
+  closeButton,
+  confirmEvent,
+}: BottomSheetProps) => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const imageCount = 25;
+
+  const handleImageClick = (index: any) => {
+    setSelectedImage(index);
+    console.log(selectedImage);
+  };
+
+  const groupedImages = Array.from(
+    { length: Math.ceil(imageCount / 5) },
+    (_, groupIndex) =>
+      Array.from({ length: 5 }, (_, innerIndex) => groupIndex * 5 + innerIndex)
+  );
+
+  if (!open) return null;
   return (
-    <div className="flex flex-col justify-start items-start overflow-hidden rounded-tl-2xl rounded-tr-2xl">
-      <div className="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0">
-        <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 w-[360px] relative gap-0.5 p-4 bg-white">
-          <p className="flex-grow-0 flex-shrink-0 text-base font-bold text-left text-black">
-            카드 꾸미기
-          </p>
+    <>
+      <div className="fixed top-0 left-0 right-0 bottom-0 bg-black/50 z-[60]" />
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white z-[70] w-[360px] rounded-xl">
+        <div className="flex flex-col items-center justify-center py-3 gap-y-2">
+          <h2 className="font-semibold">카드꾸미기</h2>
         </div>
-        <div className="flex justify-start items-start flex-grow-0 flex-shrink-0 w-[360px] gap-2 px-4 bg-white">
-          <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 w-[328px] relative gap-4 p-6 rounded-xl bg-white border border-[#e3e3e3]">
+        <div className="flex justify-start items-start flex-grow-0 flex-shrink-0 w-[360px] gap-2 px-4 bg-white pb-2">
+          <div
+            className={`flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 w-[328px] relative gap-4 p-6 rounded-xl `}
+            style={{
+              backgroundImage:
+                selectedImage !== null
+                  ? `url('/images/bg_image${selectedImage + 1}.jpg')`
+                  : "none",
+              backgroundSize: "cover", // 배경 이미지 크기 조절 (선택적)
+            }}
+          >
             <div className="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative gap-3">
               <div className="flex justify-start items-start flex-grow-0 flex-shrink-0 relative gap-0.5">
                 <p className="flex-grow-0 flex-shrink-0 text-[13px] font-medium text-left text-[#666]">
@@ -19,11 +65,11 @@ const BottomSheet = () => {
                 나는 나로 살기로 했다
               </p>
             </div>
-            <pre>
+            <p>
               Failed to transform TEXT Ich bitte die Kinder um Verzeihung, daß
               ich dieses Buch einem Erwachsenen widme. Ich habe eine ernstliche
               Entschu ldigung dsdafür: Dieser Erwa
-            </pre>
+            </p>
             <div className="flex justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5">
               <p className="self-stretch flex-grow-0 flex-shrink-0 w-[280px] text-[11px] font-light text-left text-[#666]">
                 - 앙투안 드 생텍쥐페리
@@ -31,31 +77,57 @@ const BottomSheet = () => {
             </div>
           </div>
         </div>
-        <div className="flex justify-start items-start flex-grow-0 flex-shrink-0 w-[360px] h-[164px] overflow-hidden gap-2 p-4 bg-white border-t-0 border-r-0 border-b border-l-0 border-[#e3e3e3]">
-          <div className="flex justify-start items-start flex-grow-0 flex-shrink-0 w-[328px] relative gap-2">
-            <div className="flex-grow-0 flex-shrink-0 w-[60px] h-[60px] rounded-lg bg-white border-[3px] border-[#515151]" />
-            <div className="flex-grow-0 flex-shrink-0 w-[60px] h-[60px] rounded-lg bg-[#fff1f1]" />
-            <div className="flex-grow-0 flex-shrink-0 w-[58px] h-[60px] rounded-lg bg-[#e1f3e2]" />
-            <div className="flex-grow-0 flex-shrink-0 w-[60px] h-[60px] rounded-lg bg-[#f4f4f4]" />
-            <div className="flex-grow-0 flex-shrink-0 w-[58px] h-[60px] rounded-lg bg-[#fdfdd0]" />
-            <div className="flex-grow-0 flex-shrink-0 w-[60px] h-[60px] rounded-lg bg-[#f4f0ff]" />
-            <div className="flex-grow-0 flex-shrink-0 w-[60px] h-[60px] rounded-lg bg-[#e6f8fc]" />
+
+        <div className="flex justify-start items-start flex-grow-0 flex-shrink-0 w-[360px] h-[170px] overflow-auto gap-2 p-4 bg-white border-t-0 border-r-0 border-b border-l-0 border-[#e3e3e3]">
+          <div className="justify-start items-start flex-grow-0 flex-shrink-0 w-[328px] relative gap-2">
+            {groupedImages.map((group, groupIndex) => (
+              <div key={groupIndex} className="flex gap-2 pb-2">
+                {group.map((index) => (
+                  <div
+                    key={index}
+                    className={`relative cursor-pointer `}
+                    onClick={() => handleImageClick(index)}
+                  >
+                    {/* 이미지를 가져오는 로직을 추가하세요 */}
+                    <Image
+                      src={`/images/bg_image${index + 1}.jpg`} // 예제로 사용한 임시 이미지 URL
+                      alt={`Image ${index + 1}`}
+                      width={60}
+                      height={60}
+                      className={`w-[60px] h-[60px] rounded-lg opacity-60  ${
+                        selectedImage === index
+                          ? "border-2 rounded-lg border-black	"
+                          : ""
+                      }`}
+                    />
+                  </div>
+                ))}
+              </div>
+            ))}
           </div>
         </div>
-      </div>
-      <div className="flex justify-start items-start flex-grow-0 flex-shrink-0 w-[360px] overflow-hidden gap-2 px-4 py-3 bg-white border-t-0 border-r-0 border-b border-l-0 border-[#e3e3e3]">
-        <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 w-32 relative gap-1 p-4 rounded-lg border border-[#afafaf]">
-          <p className="flex-grow-0 flex-shrink-0 text-[15px] font-bold text-left text-[#6d6d6d]">
-            취소
-          </p>
+
+        <div
+          className={`${
+            confirmButton ? "grid-cols-2" : "grid-cols-1"
+          } grid py-4`}
+        >
+          {closeButton && (
+            <button onClick={onClose} className="font-semibold text-sm">
+              {closeButton}
+            </button>
+          )}
+
+          {confirmButton && (
+            <div className="relative flex items-center justify-center">
+              <button className="font-semibold text-sm" onClick={confirmEvent}>
+                {confirmButton}
+              </button>
+            </div>
+          )}
         </div>
-        <div className="flex justify-center items-center flex-grow relative gap-1 p-4 rounded-lg bg-[#6d6d6d]">
-          <p className="flex-grow-0 flex-shrink-0 text-[15px] font-bold text-left text-white">
-            적용하기
-          </p>
-        </div>
       </div>
-    </div>
+    </>
   );
 };
 
